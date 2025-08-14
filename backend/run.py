9d644cef3a7a71c34a226a6b4cee27b147a57f43
Murphy_Juan <elin880508@gmail.com>
2025-08-14 19:39:39 +0800

    multiprocess

--- backend/run.py
+++ backend/run.py
@@ -3,6 +3,7 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
+    #dev
     uvicorn.run(
         "app.main:app",  # 🔑 關鍵：使用字符串路徑
         host="0.0.0.0",
@@ -11,3 +12,13 @@ if __name__ == "__main__":
         log_level="info",
         access_log=True
     )
+    #production
+    # uvicorn.run(
+    #     "app.main:app",
+    #     host="0.0.0.0",
+    #     port=5174,
+    #     workers=4,  # 🔑 指定進程數
+    #     # reload=True,  # ❌ 多進程模式下不能用 reload
+    #     log_level="info",
+    #     access_log=True
+    # )
